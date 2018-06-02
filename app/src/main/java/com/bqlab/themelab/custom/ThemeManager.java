06961ab7c0fa1482dc63b39591196810dec77714
bqlab <beaqon@naver.com>
2018-06-02 23:53:41 +0900

    patch-180602
     - c0001~3 url 수정

--- app/src/main/java/com/bqlab/themelab/custom/ThemeManager.java
+++ app/src/main/java/com/bqlab/themelab/custom/ThemeManager.java
@@ -22,7 +22,7 @@ public class ThemeManager {
         ThemeView c0003 = new ThemeView(context);
         c0003.setThemeName("iTalk(수동설치)");
         c0003.setThemeText("아이폰 메시지 테마의 구버전입니다. 아이폰을 부러워하지 마시고 이 테마를 설치해보세요.");
-        c0003.setThemeDownloadButton(context, "c0003", "https://drive.google.com/file/d/16iFC_87Guum-bFOL-ebZvCl3CdStLc7X/view?usp=sharing");
+        c0003.setThemeDownloadButton(context, "c0003", "https://drive.google.com/file/d/0B2pLMd1a10rwV0ItNGdjME1YQms/view?usp=sharing");
         c0003.setThemePreviewButton(context, new ArrayList<Integer>(Arrays.asList(R.drawable.c0003_image01, R.drawable.c0003_image02, R.drawable.c0003_image03, R.drawable.c0003_image04)));
         c0003.setThemePreviewImage(R.drawable.c0003_s_image01, R.drawable.c0003_s_image02, R.drawable.c0003_s_image03, R.drawable.c0003_s_image04);
         c0003.setTags(new ArrayList<String>(Arrays.asList("스타일테마", "애플", "아이폰", "아이메시지", "imessage", "심플", "iphone", "심플테마", "아이폰테마", "italk")));
@@ -32,7 +32,7 @@ public class ThemeManager {
         ThemeView c0002 = new ThemeView(context);
         c0002.setThemeName("페이스톡(수동설치)");
         c0002.setThemeText("페이스북 테마의 구버전입니다. 페이스북의 세련된 인터페이스와 디자인을 카카오톡에 담았습니다.");
-        c0002.setThemeDownloadButton(context, "c0002", "https://drive.google.com/file/d/1LSySQGOkTrPFjgSwBFuVutbD8-g-liDG/view?usp=sharing");
+        c0002.setThemeDownloadButton(context, "c0002", "https://drive.google.com/file/d/0B2pLMd1a10rwZUU4dHBLSDVqNWs/view?usp=sharing");
         c0002.setThemePreviewButton(context, new ArrayList<Integer>(Arrays.asList(R.drawable.c0002_image01, R.drawable.c0002_image02, R.drawable.c0002_image03, R.drawable.c0002_image04)));
         c0002.setThemePreviewImage(R.drawable.c0002_s_image01, R.drawable.c0002_s_image02, R.drawable.c0002_s_image03, R.drawable.c0002_s_image04);
         c0002.setTags(new ArrayList<String>(Arrays.asList("스타일테마", "페이스톡", "페이스북", "페북", "facebook", "심플", "블루", "심플테마", "카톡테마", "페북테마")));
@@ -42,7 +42,7 @@ public class ThemeManager {
         ThemeView c0001 = new ThemeView(context);
         c0001.setThemeName("인스타톡(수동설치)");
         c0001.setThemeText("인스타그램 테마의 구버전입니다. 인스타그램의 그라데이션과 심플함을 카카오톡에서 느껴보세요.");
-        c0001.setThemeDownloadButton(context, "c0001", "https://drive.google.com/file/d/1PvtHXtH5qq5JJL6K_4h9PWUFA2-x7cg-/view?usp=sharing");
+        c0001.setThemeDownloadButton(context, "c0001", "https://drive.google.com/file/d/0B2pLMd1a10rwYkM2N3h4bDhzRmc/view?usp=sharing");
         c0001.setThemePreviewButton(context, new ArrayList<Integer>(Arrays.asList(R.drawable.c0001_image01, R.drawable.c0001_image02, R.drawable.c0001_image03, R.drawable.c0001_image04)));
         c0001.setThemePreviewImage(R.drawable.c0001_s_image01, R.drawable.c0001_s_image02, R.drawable.c0001_s_image03, R.drawable.c0001_s_image04);
         c0001.setTags(new ArrayList<String>(Arrays.asList("스타일테마", "인스타톡", "인스타그램", "인스타", "인별", "instagram", "심플", "심플테마", "카톡테마", "인스타테마")));
